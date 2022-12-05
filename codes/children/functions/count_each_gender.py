f9c0b9aa2543da0dd7c8128968652e85293db1f7
whalesofheaven <78710655+whalesofheaven@users.noreply.github.com>
2022-12-05 16:29:23 -0600

    thru count_each(each_place_words)

--- codes/children/functions/count_each_gender.py
+++ codes/children/functions/count_each_gender.py
@@ -22,3 +22,15 @@ def count_each_place(Each_place_words):
         count_each_place[e] = Counter(Each_place_words[e]).most_common()
     each_place_data = pd.DataFrame(list(count_each_place.items()),columns=['place_id','frequent words'])
     return each_place_data
+
+
+'''
+    Input: words in each places
+    Output: counted data in each places
+'''
+def count_each(Each_place_words):
+    count_each_place = dict()
+    for e in Each_place_words:
+        count_each_place[e] = Counter(Each_place_words[e]).most_common()
+    each_place_data = pd.DataFrame(list(count_each_place.items()),columns=['place_id','frequent words'])
+    return each_place_data
