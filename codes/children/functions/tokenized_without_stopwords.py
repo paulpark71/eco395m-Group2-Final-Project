d4467bd7efa57367b8c0754a70a7aa505c40ca90
ChiaShengTu <chiashengtu@utexas.edu>
2022-12-05 17:14:32 -0600

    correct the code tokenized-2

--- codes/children/functions/tokenized_without_stopwords.py
+++ codes/children/functions/tokenized_without_stopwords.py
@@ -21,14 +21,13 @@ gender=dict({'male':[],
             })
 
 key =""
-
-
+   
+def tokenized_without_stopwords(dataset,Stopwords_Path, Additional_Stopwords_Path ):
+    for i,e in enumerate(dataset['review_text']):
     '''
     Input: dataset, dictionary of stopwords path, that of additional stopwords path
     Ouput: dataset on tokenized without stopwords
     '''
-def tokenized_without_stopwords(dataset,Stopwords_Path, Additional_Stopwords_Path ):
-    for i,e in enumerate(dataset['review_text']):
         if e=="":
             dataset['review_text'][i]= ""
     dataset =dataset.dropna(subset = ['review_text']).reset_index(drop=True)
