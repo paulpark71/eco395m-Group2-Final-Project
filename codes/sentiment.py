a911eb03e1d80c796b334cb02d118aafc75b109b
jacklhw <hliu799@utexas.edu>
2022-12-05 17:08:00 -0600

    Sentiment.py Finished Again

--- codes/sentiment.py
+++ codes/sentiment.py
@@ -12,7 +12,6 @@ Sentimental_data_CSV_PATH = os.path.join(BASE_DIR,'store', 'sentimental_data.csv
 Summary_sentimental_PATH = os.path.join(BASE_DIR, 'senti_plus_count_basic.csv')
 Reviewr_PATH = os.path.join(BASE_DIR, 'reviewer_r.csv')
 
-
 dataset = pd.read_csv(Reveiws_CSV_PATH)
 dd = sentiment_analysis(dataset)
 df = review_gender(dataset)
