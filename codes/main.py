3dc29f9c8a0f7bb623edbf3502b4ce3e27cf16cc
Hasutake <78459812+THashimoto27@users.noreply.github.com>
2022-12-05 16:56:01 -0600

    Done!

---
+++ codes/main.py
@@ -0,0 +1,13 @@
+''''
+    This file is only for executing all python codes for analysis
+'''
+
+print('Now Getting Google Map Data')
+from children import googlemap
+
+print('Now Counting Review words')
+from children import wordcount
+
+from children import sentiment
+
+print('Finish!')
