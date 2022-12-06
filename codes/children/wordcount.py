30e5d5fb38ad915909c1e5888119a84fa1acacd9
Hasutake <78459812+THashimoto27@users.noreply.github.com>
2022-12-06 16:47:27 -0600

    updated

--- codes/children/wordcount.py
+++ codes/children/wordcount.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
-from .functions.tokenized_without_stopwords import tokenized_without_stopwords
-from .functions.count_each_gender import all_count, count_each_place, count_each_gender
+from functions.tokenized_without_stopwords import tokenized_without_stopwords
+from functions.count_each_gender import all_count, count_each_place, count_each_gender
 
 
 """
@@ -45,6 +45,7 @@ all_count_data = all_count(All_wordsFiltered)
 count_place_data = count_each_place(Each_place_words)
 count_gender_data = count_each_gender(Each_gender_words)
 
+
 all_count_data = pd.concat([all_count_data, count_gender_data], axis=1)
 
 
@@ -70,9 +71,9 @@ integrated_data = pd.read_csv(Integrated_CSV_PATH, index_col="place_id")
 count_data = pd.read_csv(Count_Each_CSV_PATH, index_col="place_id")
 
 count_integrated_data = pd.merge(
-    integrated_data, count_data, how="inner", on="place_id"
+    integrated_data, count_data, on="place_id", how="inner"
 )
-
+print(count_integrated_data)
 with open(
     Count_integrated_CSV_PATH,
     mode="w",
